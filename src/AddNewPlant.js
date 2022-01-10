3c2aa1fcf82bdf2fcf43e7e3efa2f9740c4589cb
manijehshirzadeh <manijeh.shirzadeh63@gmail.com>
2022-01-10 16:06:39 +1100

    added divs to have each input in separate lines. set input values to empty strings when add button is clicked. so the input fields will be cleared.

--- src/AddNewPlant.js
+++ src/AddNewPlant.js
@@ -21,30 +21,38 @@ function AddNewPlant(props) {
     e.preventDefault();
     let newPlant = { name: name, species: species, instruction: instruction };
     props.onAdd(newPlant);
-    console.log({ name: name, species: species, instruction: instruction });
+    setName("");
+    setSpecies("");
+    setInstruction("");
   };
 
   return (
     <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        placeholder="Enter plant name"
-        value={name}
-        onChange={handleNameChange}
-      />
-      <input
-        type="text"
-        placeholder="Enter plant species"
-        value={species}
-        onChange={handleSpeciesChange}
-      />
-      <textarea
-        type="text"
-        placeholder="Enter watering instructions"
-        rows="3"
-        value={instruction}
-        onChange={handleInstruction}
-      />
+      <div>
+        <input
+          type="text"
+          placeholder="Enter plant name"
+          value={name}
+          onChange={handleNameChange}
+        />{" "}
+      </div>
+      <div>
+        <input
+          type="text"
+          placeholder="Enter plant species"
+          value={species}
+          onChange={handleSpeciesChange}
+        />{" "}
+      </div>
+      <div>
+        <textarea
+          type="text"
+          placeholder="Enter watering instructions"
+          rows="3"
+          value={instruction}
+          onChange={handleInstruction}
+        />
+      </div>
       <button type="submit">Add New Plant</button>
     </form>
   );
